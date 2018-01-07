90a57d61755933c8962f4174d420fd6166c3dfda
Unknown <ondrej.dlabola@gmail.com>
2018-01-07 22:48:48 +0100

    Medevac report

---
+++ app/src/main/java/cz/hombre/tacassistant/report/MedevacReport.kt
@@ -0,0 +1,13 @@
+package cz.hombre.tacassistant.report
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cz.hombre.tacassistant.R
+
+class MedevacReport : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medevac_report)
+    }
+}
