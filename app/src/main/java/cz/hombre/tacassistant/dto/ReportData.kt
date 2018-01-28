be7696c31f7b8c74b85d38c3742f63403a25aaa1
Unknown <ondrej.dlabola@gmail.com>
2018-01-28 22:53:40 +0100

    Report dtos

---
+++ app/src/main/java/cz/hombre/tacassistant/dto/ReportData.kt
@@ -0,0 +1,30 @@
+package cz.hombre.tacassistant.dto
+
+import java.io.Serializable
+import java.util.*
+
+data class ReportData(val name: String, val lines: Array<ReportLine>) : Serializable {
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ReportData
+
+        if (name != other.name) return false
+        if (!Arrays.equals(lines, other.lines)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + Arrays.hashCode(lines)
+        return result
+    }
+}
+
+data class ReportLine(val description: String, val value: String) : Serializable {
+    constructor(value: String) : this("", value)
+}
