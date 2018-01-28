f7d9dba050c6580f8b9df2efc845ed32b8d552ac
Unknown <ondrej.dlabola@gmail.com>
2018-01-28 22:54:05 +0100

    Preview report a SALTR processing data

--- app/src/main/java/cz/hombre/tacassistant/ReportPreviewActivity.kt
+++ app/src/main/java/cz/hombre/tacassistant/ReportPreviewActivity.kt
@@ -3,20 +3,33 @@ package cz.hombre.tacassistant
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import cz.hombre.tacassistant.dto.ReportData
 
 import kotlinx.android.synthetic.main.activity_report_preview.*
+import kotlinx.android.synthetic.main.content_report_preview.*
 
 class ReportPreviewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_preview)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+        fabTop.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        fabBottom.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        val report = intent?.getSerializableExtra("report") as ReportData
+        setReportPreview(report)
+    }
+
+    private fun setReportPreview(report: ReportData) {
+        preview_content.setText(report.toString()) //TODO OD - improve serialization - in ReportData
     }
 
 }
--- app/src/main/java/cz/hombre/tacassistant/report/SaltrReport.kt
+++ app/src/main/java/cz/hombre/tacassistant/report/SaltrReport.kt
@@ -1,11 +1,15 @@
 package cz.hombre.tacassistant.report
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import cz.hombre.tacassistant.R
+import cz.hombre.tacassistant.ReportPreviewActivity
+import cz.hombre.tacassistant.dto.ReportData
+import cz.hombre.tacassistant.dto.ReportLine
 
 import kotlinx.android.synthetic.main.activity_saltr_report.*
+import kotlinx.android.synthetic.main.content_saltr_report.*
 
 class SaltrReport : AppCompatActivity() {
 
@@ -14,10 +18,24 @@ class SaltrReport : AppCompatActivity() {
         setContentView(R.layout.activity_saltr_report)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val report = getReportData()
+            val previewIntent = Intent(this, ReportPreviewActivity::class.java)
+            previewIntent.putExtra("report", report)
+            startActivity(previewIntent)
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun getReportData(): ReportData {
+        val size = ReportLine(value_size.text.toString())
+        val activity = ReportLine(value_activity.text.toString())
+        val location = ReportLine(value_location.text.toString())
+        val time = ReportLine(value_time.text.toString())
+        val request = ReportLine(value_request.text.toString())
+
+        val reportData = ReportData("SALTR report", arrayOf(size, activity, location, time, request))
+
+        return reportData
     }
 
 }
