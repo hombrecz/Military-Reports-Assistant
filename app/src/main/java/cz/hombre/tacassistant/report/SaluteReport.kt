ca919618a25bc9f9af7f3f582b882900db6bb8cf
Unknown <ondrej.dlabola@gmail.com>
2018-01-30 17:35:27 +0100

    SALUTE processing data

--- app/src/main/java/cz/hombre/tacassistant/report/SaluteReport.kt
+++ app/src/main/java/cz/hombre/tacassistant/report/SaluteReport.kt
@@ -1,11 +1,15 @@
 package cz.hombre.tacassistant.report
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import cz.hombre.tacassistant.R
+import cz.hombre.tacassistant.ReportPreviewActivity
+import cz.hombre.tacassistant.dto.ReportData
+import cz.hombre.tacassistant.dto.ReportLine
 
 import kotlinx.android.synthetic.main.activity_salute_report.*
+import kotlinx.android.synthetic.main.content_salute_report.*
 
 class SaluteReport : AppCompatActivity() {
 
@@ -14,10 +18,24 @@ class SaluteReport : AppCompatActivity() {
         setContentView(R.layout.activity_salute_report)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val report = getReportData()
+            val previewIntent = Intent(this, ReportPreviewActivity::class.java)
+            previewIntent.putExtra("report", report)
+            startActivity(previewIntent)
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    private fun getReportData(): ReportData {
+        val size = ReportLine(salute_value_size.text.toString())
+        val activity = ReportLine(salute_value_activity.text.toString())
+        val location = ReportLine(salute_value_location.text.toString())
+        val uniform = ReportLine(salute_uniform_value.text.toString())
+        val time = ReportLine(salute_value_time.text.toString())
+        val equipment = ReportLine(salute_value_equipment.text.toString())
+
+        val reportData = ReportData("SALTR report", arrayOf(size, activity, location, uniform, time, equipment))
+
+        return reportData
+    }
 }
