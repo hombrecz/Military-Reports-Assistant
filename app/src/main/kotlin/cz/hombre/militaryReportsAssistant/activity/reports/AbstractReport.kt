c413de45ff96eef454838282903d3b8991a71412
Unknown <ondrej.dlabola@gmail.com>
2018-04-16 23:58:57 +0200

    Wrning dialog begore leaving report concept

--- app/src/main/kotlin/cz/hombre/militaryReportsAssistant/activity/reports/AbstractReport.kt
+++ app/src/main/kotlin/cz/hombre/militaryReportsAssistant/activity/reports/AbstractReport.kt
@@ -3,12 +3,16 @@ package cz.hombre.militaryReportsAssistant.activity.reports
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
+import cz.hombre.militaryReportsAssistant.R
 import cz.hombre.militaryReportsAssistant.Utilities.Companion.REPORT_PROPERTY
 import cz.hombre.militaryReportsAssistant.dto.ReportData
 import cz.hombre.militaryReportsAssistant.services.DateTimeService
 import cz.hombre.militaryReportsAssistant.services.LocaleService
 import cz.hombre.militaryReportsAssistant.services.LocationService
 import cz.hombre.militaryReportsAssistant.services.PreferencesService
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.yesButton
 import org.koin.android.ext.android.inject
 
 abstract class AbstractReport : AppCompatActivity() {
@@ -29,5 +33,13 @@ abstract class AbstractReport : AppCompatActivity() {
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(localeService.setPreferredLocale(base))
+    }
+
+    override fun onBackPressed() {
+        alert(localeService.getStringForActualLocale(baseContext, R.string.report_leaving_dialog_content),
+                localeService.getStringForActualLocale(baseContext, R.string.report_leaving_dialog_label)) {
+            yesButton { super.onBackPressed() }
+            noButton {}
+        }.show()
     }
 }
