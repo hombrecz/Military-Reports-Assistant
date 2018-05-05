bff36509bf53566509780bf21cb1cd07d8ff53fe
Unknown <ondrej.dlabola@gmail.com>
2018-05-05 16:43:49 +0200

    Updated dependencies and improved SITREP

--- app/src/main/kotlin/cz/hombre/militaryReportsAssistant/activity/reports/SituationReport.kt
+++ app/src/main/kotlin/cz/hombre/militaryReportsAssistant/activity/reports/SituationReport.kt
@@ -1,5 +1,6 @@
 package cz.hombre.militaryReportsAssistant.activity.reports
 
+import android.content.Intent
 import android.os.Bundle
 import cz.hombre.militaryReportsAssistant.R
 import cz.hombre.militaryReportsAssistant.dto.ReportData
@@ -9,7 +10,7 @@ import org.jetbrains.anko.setContentView
 
 class SituationReport : AbstractReport() {
 
-    private var situationReportUI = SituationReportUI(dateTimeService)
+    private var situationReportUI = SituationReportUI(dateTimeService, locationService)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,17 +21,30 @@ class SituationReport : AbstractReport() {
             translateReport()
         }
         situationReportUI.time.setValue(dateTimeService.getMilitaryDateTimeGroup())
+        situationReportUI.location.setValue(locationService.getCurrentMGRSLocation())
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data != null) {
+            situationReportUI.location.onLocationSelected(requestCode, resultCode, data)
+        }
     }
 
     override fun getReportData(): ReportData {
 
         val time = ReportLine(situationReportUI.time.getValue())
-        val status = ReportLine(situationReportUI.status.getValue())
         val enemy = ReportLine(situationReportUI.enemy.getValue())
+        val location = ReportLine(situationReportUI.location.getValue())
         val own = ReportLine(situationReportUI.own.getValue())
+        val vehicles = ReportLine(situationReportUI.vehicles.getValue())
+        val status = ReportLine(situationReportUI.status.getValue())
+        val ammunition = ReportLine(situationReportUI.ammunition.getValue())
+        val fuel = ReportLine(situationReportUI.fuel.getValue())
         val follow = ReportLine(situationReportUI.following.getValue())
 
-        return ReportData(getString(R.string.title_activity_situation_report), arrayOf(time, status, enemy, own, follow))
+        return ReportData(getString(R.string.title_activity_situation_report), arrayOf(time, enemy, location, own, vehicles, status, ammunition,
+                fuel, follow))
     }
 }
--- app/src/main/kotlin/cz/hombre/militaryReportsAssistant/layout/reports/SituationReportUI.kt
+++ app/src/main/kotlin/cz/hombre/militaryReportsAssistant/layout/reports/SituationReportUI.kt
@@ -7,17 +7,22 @@ import cz.hombre.militaryReportsAssistant.R.string.*
 import cz.hombre.militaryReportsAssistant.activity.reports.SituationReport
 import cz.hombre.militaryReportsAssistant.layout.components.*
 import cz.hombre.militaryReportsAssistant.services.DateTimeService
+import cz.hombre.militaryReportsAssistant.services.LocationService
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.coordinatorLayout
 import org.jetbrains.anko.design.floatingActionButton
 
-class SituationReportUI(private val dateTimeService: DateTimeService) : AnkoComponent<SituationReport> {
+class SituationReportUI(private val dateTimeService: DateTimeService, private val locationService: LocationService) : AnkoComponent<SituationReport> {
 
     lateinit var previewButton: FloatingActionButton
     lateinit var time: TimeInput
-    lateinit var status: RadioInput
     lateinit var enemy: TextInput
+    lateinit var location: LocationInput
     lateinit var own: TextInput
+    lateinit var vehicles: TextInput
+    lateinit var status: RadioInput
+    lateinit var ammunition: RadioInput
+    lateinit var fuel: RadioInput
     lateinit var following: TextInput
 
     override fun createView(ui: AnkoContext<SituationReport>) = with(ui) {
@@ -26,9 +31,13 @@ class SituationReportUI(private val dateTimeService: DateTimeService) : AnkoComp
             scrollView {
                 verticalLayout {
                     time = timeInput(R.string.report_time, R.string.report_time_hint, dateTimeService)
-                    status = radioInput(R.string.report_situation_status, statusOptions())
                     enemy = textInput(R.string.report_situation_enemy, R.string.report_situation_enemy_hint)
+                    location = locationInput(report_situation_location, report_situation_location_hint, locationService, ui.owner)
                     own = textInput(R.string.report_situation_own, R.string.report_situation_own_hint)
+                    vehicles = textInput(report_situation_vehicles, report_situation_vehicles_hint)
+                    status = radioInput(R.string.report_situation_status, unitStatusOptions())
+                    ammunition = radioInput(report_situation_ammunition, statusOptions())
+                    fuel = radioInput(report_situation_fuel, statusOptions())
                     following = textInput(R.string.report_situation_following, R.string.report_situation_following_hint)
                 }
                 padding = dip(10)
@@ -46,6 +55,12 @@ class SituationReportUI(private val dateTimeService: DateTimeService) : AnkoComp
             }
         }
     }
+
+    private fun unitStatusOptions() = linkedMapOf(
+            report_situation_status_green to report_situation_unit_status_green_label,
+            report_situation_status_amber to report_situation_unit_status_amber_label,
+            report_situation_status_red to report_situation_unit_status_red_label,
+            report_situation_status_black to report_situation_unit_status_black_label)
 
     private fun statusOptions() = linkedMapOf(
             report_situation_status_green to report_situation_status_green_label,
